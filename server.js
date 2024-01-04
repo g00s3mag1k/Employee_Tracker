9f5cae3475f4a40bc86dfbdd4dec1ad0a3c22158
Brandon <beegee54321@gmail.com>
2024-01-04 03:45:47 -0800

    made function for command prompt app/inquirer prompt

--- server.js
+++ server.js
@@ -1,20 +1,64 @@
-const express = require('express');
+const inquirer = require('inquirer');
 const mysql = require('mysql2');
 //Import and require mysql2
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-//Express middleware
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json());
 
 //Connect to database
 const db = mysql.createConnection(
     {
         host: 'localhost',
+        port: 3306,
         user: 'root',
         password: '',
-        database: ''
+        database: 'employee_db'
     },
     console.log(`Connected to the dtabse.`)
 );
+
+connection.connect ((err) => {
+    if (err) throw err;
+    console.log('You have successfully connected!');
+});
+
+//Function to start the app
+function strTracker() {
+    inquirer
+     .prompt ({
+        name: 'action',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: [
+            'View all departments',
+            'View all roles',
+            'View all employess',
+            'Add a department',
+            'Add a role',
+            'Add an employee',
+            'Update an employee role',
+            'Exit'
+        ]
+
+     })
+     .then ((answer) => {
+        switch (answer.action) {
+            case 'View all departments':
+                break;
+            case 'View all roles':
+                break;
+            case 'View all employees':
+                break;
+            case 'Add a department':
+                break;
+            case 'Add a role':
+                break;
+            case 'Add an employee':
+                break;
+            case 'Update an employee role':
+                break;
+            case 'Exit':
+                break;
+            
+        }
+     });
+}
+
+//Function to view all departments
