b2833b7c985ecb931a49d80e02057aa56d407fc7
Brandon <beegee54321@gmail.com>
2024-01-09 23:15:05 -0800

    linking and functionality issues still needs resolve

--- server.js
+++ server.js
@@ -47,10 +47,10 @@ async function init() {
             addRole();
             break;
         case 'Add an employee':
-            addEmployee(); // Fixed: added the missing function call
+            
             break;
         case 'Update an employee role':
-            updateEmployeeRole(); // Fixed: added the missing function call
+            
             break;
         case 'Exit':
             db.end();
@@ -85,19 +85,7 @@ async function viewAllRoles() {
 async function viewAllEmployees() {
     try {
         const results = db.query(`
-            SELECT
-                employee.id,
-                employee.first_name,
-                employee.last_name,
-                role.title,
-                department.name AS department,
-                role.salary,
-                CONCAT(manager.first_name, " ", manager.last_name) AS manager
-            FROM
-                employee
-            INNER JOIN role ON employee.role_id = role.id
-            INNER JOIN department ON role.department_id = department.id
-            LEFT JOIN employee manager ON manager.id = employee.manager_id
+            SELECT * from employee
         `);
         console.table(results);
     } catch (err) {
